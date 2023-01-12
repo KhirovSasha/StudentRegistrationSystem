f624bfde09f06a41a6d75006a5635c20a6519126
Khiro Sasha <sasha.khiro@gmail.com>
2023-01-12 17:07:36 +0200

    created View Model and Dto for user registration

---
+++ Backend/BusinessLogic/Dto/AuthDto/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessLogic.Dto.AuthDto
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nikname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
