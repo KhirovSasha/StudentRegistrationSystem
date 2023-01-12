d7f6f2a033bc03745574b7c656d63be0ee037162
Khiro Sasha <sasha.khiro@gmail.com>
2023-01-12 17:08:14 +0200

    Aded AutoMapper

---
+++ Backend/BusinessLogic/MapperProfilers/AppMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BusinessLogic.Dto.AuthDto;
+using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.MapperProfilers
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile() 
+        {
+            CreateMap<RegisterDto, User>()
+                .ForMember(user => user.FullName, opt => opt.MapFrom(user => $"{user.FirstName} {user.LastName}"));
+        }
+    }
+}
