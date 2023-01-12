ba134dee69dbc35f9479779362f090450728a4be
Khiro Sasha <sasha.khiro@gmail.com>
2023-01-12 17:12:01 +0200

    aded Countroller with end point register

---
+++ Backend/API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLogic.Dto.AuthDto;
+using BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegisterDto>> Register([FromBody] RegisterDto user)
+        {
+            var createdUser = await authService.Register(user);
+
+            if (!createdUser)
+            {
+                return BadRequest("Your date not valide");
+            }
+
+            return Ok("Congratulations, you are successfully registered on.");
+        }
+    }
+}
